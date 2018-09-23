d47aedd0e5cf4982150b6d85d63cfdd112d2dbc2
visampaio <viviane.sm3@gmail.com>
2018-09-23 13:35:12 -0400

    Add finished function to filter by number of players

--- sort.js
+++ sort.js
@@ -1,23 +1,41 @@
 const file = document.getElementById('input');
-var selPlayer = document.getElementById('selectPlayer');
+const selPlayer = document.getElementById('selectPlayer');
+const choose = document.getElementById('choose');
 var bgList;
-var filteredList = [];
 
 file.addEventListener("change", function() {
   Papa.parse(file.files[0], {
     header: true,
     complete: function(results) {
       bgList = results.data;
-  		console.log(bgList);
+      splitPlayers(bgList);
   	}
   });
 });
 
-function filterPlayer(){
-  for(var i=0; i<bgList.length; i++) {
-    if (bgList[i].Players == selPlayer.value) {
-      filteredList.push(bgList[i].Game);
+choose.addEventListener("click", function() {
+  console.log(filter(bgList));
+});
+
+function filter(bgList){
+  return filterPlayer(bgList);
+}
+
+function filterPlayer(bgList){
+  var filteredList = [];
+
+  for(var i=0; i<bgList.length-1; i++) {
+    for(var j=0; j< bgList[i].Players.length; j++) {
+      if (bgList[i].Players[j] == selPlayer.value) {
+        filteredList.push(bgList[i].Game);
+      }
     }
   }
-  console.log(filteredList)
+  return filteredList;
+}
+
+function splitPlayers(games){
+  for(var i=0; i<games.length-1; i++) {
+    games[i].Players = games[i].Players.split(",");
+  };
 }
