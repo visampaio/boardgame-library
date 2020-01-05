94dbd2eed32024b6e33005719bf42133658acec6
Viviane Sampaio Maia <viviane.sm3@gmail.com>
2020-01-04 21:45:48 -0500

    Update storage.js

--- storage.js
+++ storage.js
@@ -15,6 +15,9 @@ var bgList = [{
 
 var movelIndex = 0;
 var moveis = [];
+var highlightedFurniture = [];
+var highlightedPrateleira = [];
+var highlightedColuna = [];
 
 // function Gersby(number, shelf, column, position, name) {
 //   for (var i=0, i<)
@@ -31,24 +34,89 @@ var moveis = [];
 // }
 function sortPosition() {
   for (i=0; i<bgList[0].Position.length; i++) {
-    filterFurniture(i);
+    filterFurnitureType(i);
     movelIndex++;
   }
 }
 
 
-function filterFurniture(movelIndex) {
-  for (var i=0; i<bgList.length; i++) {
-    if (moveis.filter(movel => (movel.name == bgList[i].Position[movelIndex])).length > 0) {
-      moveis[moveis.findIndex(movel => movel.name == bgList[i].Position[movelIndex])].array.push(bgList[i]);
+function filterFurnitureType() {
+  for (var i=0; i<bgList.length-1; i++) {
+    if (moveis.filter(movel => (movel.name == bgList[i].Position[0])).length > 0) {
+      moveis[moveis.findIndex(movel => movel.name == bgList[i].Position[0])].array.push(bgList[i]);
     }
     else {
-      var movelTipo = {name: bgList[i].Position[movelIndex], array: []};
+      var movelTipo = {name: bgList[i].Position[0], array: []};
       moveis.push(movelTipo);
-      moveis[moveis.findIndex(movel => movel.name == bgList[i].Position[movelIndex])].array.push(bgList[i]);
+      moveis[moveis.findIndex(movel => movel.name == bgList[i].Position[0])].array.push(bgList[i]);
     }
   }
 }
+
+function filterFurniture() {
+  for (var i=0; i<moveis.length; i++) {
+    highlightedFurniture = moveis[i].array;
+    // console.log("Iteracao numeross" + i + ": " + JSON.stringify(highlightedFurniture));
+    var originalSize = 0;
+    var indexJ = highlightedFurniture.length;
+    for (var j=indexJ-1; j>=0; j--) {
+      // console.log("Iteracao numero" + i + ": " + JSON.stringify(highlightedFurniture));
+      if (highlightedFurniture.filter(movel => (movel.name == highlightedFurniture[j].Position[1])).length > 0) {
+        highlightedFurniture[highlightedFurniture.findIndex(movel => movel.name == highlightedFurniture[j].Position[1])].array.push(highlightedFurniture[j]);
+        // highlightedFurniture.splice(j,1);
+      }
+      else {
+        var prateleiraTipo = {name: highlightedFurniture[j].Position[1], array: []};
+        highlightedFurniture.push(prateleiraTipo);
+        highlightedFurniture[highlightedFurniture.findIndex(movel => movel.name == highlightedFurniture[j].Position[1])].array.push(highlightedFurniture[j]);
+        // highlightedFurniture.splice(j,1);
+      }
+    }
+    highlightedFurniture.splice(0,indexJ);
+  }
+}
+
+function filterPrateleira() {
+  for (var i=0; i<highlightedFurniture.length; i++) {
+    highlightedPrateleira = highlightedFurniture[i].array;
+    var originalSize = 0;
+    for (var j=highlightedPrateleira.length-1; j>=0; j--) {
+      if (highlightedPrateleira.filter(movel => (movel.name == highlightedPrateleira[j].Position[2])).length > 0) {
+        highlightedPrateleira[highlightedPrateleira.findIndex(movel => movel.name == highlightedPrateleira[j].Position[1])].array.push(highlightedPrateleira[j]);
+        highlightedPrateleira.splice(j,1);
+      }
+      else {
+        var prateleiraTipo = {name: highlightedPrateleira[j].Position[2], array: []};
+        highlightedPrateleira.push(prateleiraTipo);
+        highlightedPrateleira[highlightedPrateleira.findIndex(movel => movel.name == highlightedPrateleira[j].Position[2])].array.push(highlightedPrateleira[j]);
+        highlightedPrateleira.splice(j,1);
+      }
+    }
+
+  }
+}
+
+function filterColuna() {
+  for (var i=0; i<moveis.length; i++) {
+    var highlightedFurniture = moveis[i].array;
+    console.log(highlightedFurniture);
+    var originalSize = 0;
+    for (var j=highlightedFurniture.length-1; j>=0; j--) {
+      if (highlightedFurniture.filter(movel => (movel.name == highlightedFurniture[j].Position[1])).length > 0) {
+        highlightedFurniture[highlightedFurniture.findIndex(movel => movel.name == highlightedFurniture[j].Position[1])].array.push(highlightedFurniture[j]);
+        highlightedFurniture.splice(j,1);
+      }
+      else {
+        var prateleiraTipo = {name: highlightedFurniture[j].Position[1], array: []};
+        highlightedFurniture.push(prateleiraTipo);
+        highlightedFurniture[highlightedFurniture.findIndex(movel => movel.name == highlightedFurniture[j].Position[1])].array.push(highlightedFurniture[j]);
+        highlightedFurniture.splice(j,1);
+      }
+    }
+
+  }
+}
+
 
 function movelCondition(movel, i) {
   return movel.name == bgList[i].Position[0];
