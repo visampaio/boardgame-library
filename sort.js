dab64b8ace6e8b33d1b0088efb831983c1dce42d
Viviane <viviane.sm3@gmail.com>
2026-01-04 11:32:36 -0500

    Fix accessibility buttons

--- sort.js
+++ sort.js
@@ -23,7 +23,7 @@ var filteredList = [];
 var selected = [];
 var chosenList = [];
 var fullTemplate = "<div class='container'><img height='' alt='' src='' class='GamePicture'><div class='overlay'><div class='linkA'><a class='GameLink' href='' target='_blank'><img class='logos' src='logos/bgglogo-original.png' /></a></div><div class='linkA'><a class='GameVideoLink' href='' target='_blank'><img class='logos' src='logos/watchitplayed-original.jpeg' /></a></div></div></div><p id='GameName'>Title: </p><p id='GamePlayers'>Number of Players: </p><p id='GameTime'>Game Length: </p><p id='GameComplexity'>Game Complexity: </p><p id='GamePlayed'>Previously Played: </p><p id='GameMode'>Game Type: </p><p id='GameLocation'>Game Location: </p>";
-var picturesHeight = 200;
+var picturesHeight = "180px";
 var revUnplayedOriginal;
 
 document.getElementById("pageright").addEventListener("click", function() {
@@ -37,29 +37,39 @@ document.getElementById("pageleft").addEventListener("click", function() {
 });
 
 // Accessibility Buttons: Increase, Decrease, and reset Image sizes
+const pictures = document.getElementsByClassName("container");
+
   increaseImg.addEventListener("click", function() {
-    var pictures = document.getElementsByClassName("GamePicture");
-    for (var i=0; i<pictures.length; i++) {
-    pictures[i].height = pictures[i].height*1.1;
-  }
-    picturesHeight = pictures[0].height;
+    let newSize = pictures[0].offsetHeight*1.1 + "px";
+    changeImgSize(newSize);
   });
 
   decreaseImg.addEventListener("click", function() {
-    var pictures = document.getElementsByClassName("GamePicture");
-    for (var i=0; i<pictures.length; i++) {
-    pictures[i].height = pictures[i].height*0.9;
-  }
-    picturesHeight = pictures[0].height;
+    let newSize = pictures[0].offsetHeight*0.9 + "px";
+    changeImgSize(newSize);
   });
 
   resetImgSize.addEventListener("click", function() {
-    picturesHeight = 200;
-    var pictures = document.getElementsByClassName("GamePicture");
-    for (var i=0; i<pictures.length; i++) {
-    pictures[i].height = picturesHeight;
-  }
+    changeImgSize("180px");
   });
+
+   function changeImgSize(newSize) {
+    // Source: https://alvarotrigo.com/blog/change-css-javascript/
+    // Getting the stylesheet
+    const stylesheet = document.styleSheets[2];
+    let elementRules;
+
+    // looping through all its rules and getting your rule
+    for(let i = 0; i < stylesheet.cssRules.length; i++) {
+      if(stylesheet.cssRules[i].selectorText === '.container') {
+        elementRules = stylesheet.cssRules[i];
+      }
+    }
+
+    // modifying the rule in the stylesheet
+    elementRules.style.setProperty('height', newSize);
+    elementRules.style.setProperty('width', newSize);
+  }
 
 // Preserve 'Reveal Unplayed' Status when selecting 'New Games Only' Option
 checkNew.addEventListener("change", function() {
@@ -181,7 +191,7 @@ function createGamesDisplay(segmento) {
         var videoElement = segmento[z].Video ? `<div class='linkA'><a class='GameVideoLink' href='${segmento[z].Video}' target='_blank'><img class='logos' src='logos/watchitplayed-original.jpeg' /></a></div>` : ``;
         string += `
         <div class='container' title='${segmento[z].Game.replace(/'/g, '&#39;')}' onclick="chooseGame(this, this.title)" 
-        alt='${segmento[z].Game.replace(/'/g, '&#39;')}' style='background-image: url("${segmento[z].Picture}"); ${setGrayscale(segmento[z].Played)}' height='${picturesHeight}' width='${picturesHeight}'">
+        alt='${segmento[z].Game.replace(/'/g, '&#39;')}' style='background-image: url("${segmento[z].Picture}"); ${setGrayscale(segmento[z].Played)}'>
           <div class='overlay'>
             <div class='linkA'>
               <a class='GameLink' href='${segmento[z].Link}' target='_blank'><img class='logos' src='logos/bgglogo-original.png' /></a>
