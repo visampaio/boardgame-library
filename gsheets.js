cb87362f1b993bbdbbb5885b8e4339f051ef594f
Viviane <viviane.sm3@gmail.com>
2026-09-10 20:13:22 -0400

    Update Google Auth to GIS

--- gsheets.js
+++ gsheets.js
@@ -1,20 +1,43 @@
-function start() {
-  // Initializes the Google Sheets API library.
-  gapi.client.init({
-    'apiKey': 'AIzaSyDaGUPSr4asMfHt_Qlie01vzbpl8B35nBo',
-    'discoveryDocs': ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-    'clientId': '37348095959-i0nd20ns5vklh6q2drb7mf2am83vqou4.apps.googleusercontent.com',
-    'scope': 'https://www.googleapis.com/auth/spreadsheets.readonly',
-  }).then(function() {
-    parseSheet();
+const CLIENT_ID = '37348095959-i0nd20ns5vklh6q2drb7mf2am83vqou4.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDaGUPSr4asMfHt_Qlie01vzbpl8B35nBo';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+// Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+function gapiLoaded() {
+  gapi.load('client', initializeGapiClient);
+}
+
+async function initializeGapiClient() {
+   await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
   });
-};
+  gapiInited = true;
+  parseSheet();
+}
+
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: 'parseSheet', // defined later
+  });
+  gisInited = true;
+}
 
 function parseSheet() {
   // Reads values on the spreadsheet, starting from row 2.
   gapi.client.sheets.spreadsheets.values.get({
     spreadsheetId: "1ZLdvkWefFzPAZtWy8s6GG2HN17eRvJdH-LvL3Uqbeoc",
-    range: "A2:N"
+    range: "A2:N600",
   }).then((response) => {
     let completeList = response.result.values;
     let list = [];
@@ -46,6 +69,3 @@ function splitArray(array, comma) {
     return (array.length > 1) ? array.split(comma) : array;
   } else {return "";}
 }
-
-// Loads the Google Sheets API library.
-gapi.load('client', start);
