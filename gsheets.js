244fdf5beb65698f51b4c09040fc2dac1caa74ac
Viviane <viviane.sm3@gmail.com>
2026-09-12 11:57:22 -0400

    Adds catch(error) to parseSheet()

--- gsheets.js
+++ gsheets.js
@@ -10,6 +10,7 @@ const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 let tokenClient;
 let gapiInited = false;
 let gisInited = false;
+let retries = 3;
 
 function gapiLoaded() {
   gapi.load('client', initializeGapiClient);
@@ -28,27 +29,34 @@ function gisLoaded() {
   tokenClient = google.accounts.oauth2.initTokenClient({
     client_id: CLIENT_ID,
     scope: SCOPES,
-    callback: 'parseSheet', // defined later
+    callback: '', // defined later
   });
   gisInited = true;
 }
 
-function parseSheet() {
-  // Reads values on the spreadsheet, starting from row 2.
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: "1ZLdvkWefFzPAZtWy8s6GG2HN17eRvJdH-LvL3Uqbeoc",
-    range: "A2:N600",
-  }).then((response) => {
-    let completeList = response.result.values;
-    let list = [];
-    for (let i=0; i < completeList.length; i++) {
-      let game = new Game(completeList[i]);
-      list.push(game);
-    }
-    bgList = list;
+async function parseSheet() {
+  try {
+    // Reads values on the spreadsheet, starting from row 2.
+    const response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: "1ZLdvkWefFzPAZtWy8s6GG2HN17eRvJdH-LvL3Uqbeoc",
+      range: "A2:N600",
+    });
+
+  let completeList = response.result.values;
+    bgList = completeList.map(row => new Game(row));
     displayAtStart();
-  });
+  } catch (error) {
+    if (retries > 0) {
+      console.log(`Encountered ${error.status}. Retrying in 2s...`);
+      setTimeout(() => parseSheet(), 2000);
+      retries--;
+    }
+    else {
+      console.log("Failed to get sheet data.")
+    }
+  }
 }
+
 
 function Game(game) {
   this.Game = game[0];
